d0d426cc202e8baf02128605dcde0be50d1772ec
rewish <rewish@224e63c0-5457-0410-989b-907d9cf58824>
2010-09-05 17:12:37 +0000

    テスト用のinclude_pathを修正。

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,11 +5,10 @@ define('TEST_PATH_CACHE', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'caches');
 
 // Target
 define('TEST_SBM_INFO_URL',   'http://example.com/');
-define('TEST_SBM_INFO_TITLE', 'えぐざんぽー');
+define('TEST_SBM_INFO_TITLE', 'Example Web Page');
 
 // Set include path
-foreach (array(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'src',
-               dirname(__FILE__) . DIRECTORY_SEPARATOR . 'cases',
-               dirname(__FILE__) . DIRECTORY_SEPARATOR . 'mocks') as $path) {
-    set_include_path($path . PATH_SEPARATOR . get_include_path());
+set_include_path(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'src');
+foreach (array('cases', 'mocks') as $path) {
+    set_include_path(dirname(__FILE__) . DIRECTORY_SEPARATOR . $path . PATH_SEPARATOR . get_include_path());
 }
